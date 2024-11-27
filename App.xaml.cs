3d3413b1536b0bdb3f0e7f9313699d81a84dafaf
IzakCancino <84246483+IzakCancino@users.noreply.github.com>
2024-11-26 19:29:48 -0600

    Init commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace healthy_reminders
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# healthy-reminders
+Windows desktop application to get reminders to improve health while using the PC
