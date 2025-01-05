161a15363236e0d99085c0d4dd493a786f72f09b
IzakCancino <84246483+IzakCancino@users.noreply.github.com>
2025-01-05 11:35:11 -0600

    All settings for the timers added

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -28,8 +28,14 @@ namespace healthy_reminders
 
             CheckTimersOnStart.IsChecked = Properties.Settings.Default.TimersOnStart;
             CheckMinimizedOnStart.IsChecked = Properties.Settings.Default.MinimizedOnStart;
+
             InputDelayEyeCare.Text = _mainWindow.EventTimers["EyeCare"].DelayTime.TotalSeconds.ToString();
             InputEventEyeCare.Text = _mainWindow.EventTimers["EyeCare"].EventTime.TotalSeconds.ToString();
+
+            InputDelayPostureHealth.Text = _mainWindow.EventTimers["PostureHealth"].DelayTime.TotalSeconds.ToString();
+
+            InputDelayPhysicalActivity.Text = _mainWindow.EventTimers["PhysicalActivity"].DelayTime.TotalSeconds.ToString();
+            InputEventPhysicalActivity.Text = _mainWindow.EventTimers["PhysicalActivity"].EventTime.TotalSeconds.ToString();
         }
 
         private void Window_Closed(object sender, EventArgs e)
@@ -59,18 +65,52 @@ namespace healthy_reminders
                 return;
             }
 
+            if (!double.TryParse(InputDelayPostureHealth.Text, out double valueDelayPostureHealth))
+            {
+                InputDelayPostureHealth.Text = string.Empty;
+                InputDelayPostureHealth.Focus();
+                return;
+            }
+
+            if (!double.TryParse(InputDelayPhysicalActivity.Text, out double valueDelayPhysicalActivity))
+            {
+                InputDelayPhysicalActivity.Text = string.Empty;
+                InputDelayPhysicalActivity.Focus();
+                return;
+            }
+            if (!double.TryParse(InputEventPhysicalActivity.Text, out double valueEventPhysicalActivity))
+            {
+                InputEventPhysicalActivity.Text = string.Empty;
+                InputEventPhysicalActivity.Focus();
+                return;
+            }
+
             // Update timers' values
             _mainWindow.EventTimers["EyeCare"].DelayTime = TimeSpan.FromSeconds(valueDelayEyeCare);
             _mainWindow.EventTimers["EyeCare"].EventTime = TimeSpan.FromSeconds(valueEventEyeCare);
 
+            _mainWindow.EventTimers["PostureHealth"].DelayTime = TimeSpan.FromSeconds(valueDelayPostureHealth);
+
+            _mainWindow.EventTimers["PhysicalActivity"].DelayTime = TimeSpan.FromSeconds(valueDelayPhysicalActivity);
+            _mainWindow.EventTimers["PhysicalActivity"].EventTime = TimeSpan.FromSeconds(valueEventPhysicalActivity);
+
             // Reset timers
             _mainWindow.EventTimers["EyeCare"].Reset();
+            _mainWindow.EventTimers["PostureHealth"].Reset();
+            _mainWindow.EventTimers["PhysicalActivity"].Reset();
 
             // Save settings
             Properties.Settings.Default.MinimizedOnStart = CheckMinimizedOnStart.IsChecked.GetValueOrDefault();
             Properties.Settings.Default.TimersOnStart = CheckTimersOnStart.IsChecked.GetValueOrDefault();
+
             Properties.Settings.Default.TimerDelayEyeCareInSeconds = valueDelayEyeCare;
             Properties.Settings.Default.TimerEventEyeCareInSeconds = valueEventEyeCare;
+
+            Properties.Settings.Default.TimerDelayPostureHealthInSeconds = valueDelayPostureHealth;
+            
+            Properties.Settings.Default.TimerDelayPhysicalActivityInSeconds = valueDelayPhysicalActivity;
+            Properties.Settings.Default.TimerEventPhysicalActivityInSeconds = valueEventPhysicalActivity;
+
             Properties.Settings.Default.Save();            
 
             BtnCancel_Click(sender, e);
